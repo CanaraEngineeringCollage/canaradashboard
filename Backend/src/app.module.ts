502d0bff311394e988db73dddc752cc2b3acc496
Jamshadj <jamshadalij2004@gmail.com>
2025-07-07 09:16:36 +0530

    database updated

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -10,7 +10,8 @@ import { BuzzModule } from './buzz/buzz.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: parseInt(process.env.DB_PORT || '3307', 10),
+
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
