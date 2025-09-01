7bc88b04a0fb73978ccfde90d8b6e902992147b4
Jamshadj <jamshadalij2004@gmail.com>
2025-09-01 16:59:02 +0530

    updated the code for productuon

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -9,31 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('✅ NestJS application created');
   app.use(cookieParser());
-<<<<<<< HEAD
   app.enableCors({
     origin: ['http://localhost:9002', 'http://localhost:3001'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-=======
-app.enableCors({
-  origin: [
-    'https://canaradashboard-aooq.vercel.app', // Your existing origin
-    'http://localhost:3000'                     // Add your new origin here
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-});
-
-  // ✅ Swagger setup
-  const config = new DocumentBuilder()
-    .setTitle('CEC API')
-    .setDescription('Canara Engineering College API')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config, {
-    ignoreGlobalPrefix: false, // this includes /api in all routes
->>>>>>> 534fe42a0ad2222bfc280d6df4a3b36f97938da3
   });
 
   await app.listen(3000);
