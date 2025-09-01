61eabdaba711c61e2b8de0322770f36849237058
Jamshadj <jamshadalij2004@gmail.com>
2025-09-01 20:38:19 +0530

    origin updated

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   console.log('✅ NestJS application created');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:9002', 'http://localhost:3001'],
+    origin: ['http://localhost:9002', 'https://canaradashboard-aooq.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
