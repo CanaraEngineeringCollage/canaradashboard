85acb746ee1a942f2ed68daceee295b89ccae0a9
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-09-24 11:48:55 +0530

    corsFix

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { ConfigModule } from '@nestjs/config';
 import { BuzzModule } from './buzz/buzz.module';
 import { EventModule } from './events/events.module';
 import { AdminModule } from './admin/admin.module';
-import { AuthModule } from './auth/auth.module';
 import { FacultyModule } from './faculty/faculty.module';
 import { GrievanceModule } from './grievance/grievance.module';
 import { ScstGrievanceModule } from './sc-st-grievance/scst-grievance.module';
@@ -19,7 +18,7 @@ import { AlumniModule } from './alumni/alumni.module';
       type: 'mysql',
       host: process.env.DB_HOST,
       port: 3306,
-      username: process.env.DB_USER,
+      username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
@@ -28,7 +27,6 @@ import { AlumniModule } from './alumni/alumni.module';
     BuzzModule,
     EventModule,
     AdminModule,
-    AuthModule,
     FacultyModule,
     GrievanceModule,
     ScstGrievanceModule,
--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   console.log('✅ NestJS application created');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:9002', 'http://localhost:3001'],
+    origin: ['http://localhost:9002', 'http://localhost:3001','http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
