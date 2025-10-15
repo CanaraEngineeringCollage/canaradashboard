90b2a0c3f5e6fa9b560d088149ea9edce7da8fe6
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-10-15 18:10:22 +0530

    mechInEvents

--- Frontend/src/app/dashboard/events/components/EventModal/EventModal.tsx
+++ Frontend/src/app/dashboard/events/components/EventModal/EventModal.tsx
@@ -72,6 +72,7 @@ const EventModal: React.FC<Props> = ({
             <option value="Computer Science & Design">Computer Science & Design</option>
             <option value="Computer Science & Business System">Computer Science & Business System</option>
             <option value="Artificial Intelligence & Machine Learning">Artificial Intelligence & Machine Learning</option>
+            <option value="Mechanical Engineering">Mechanical Engineering</option>
             <option value="Science & Humanities">Science & Humanities</option>
              <option value="Student Welfare Department">Student Welfare Department</option>
 
